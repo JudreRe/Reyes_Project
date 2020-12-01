1472695f7a07c69e55c4ac38a8b5d3cbc8f5fc89
Judre <Reyesjudre@gmail.com>
2020-11-30 19:28:30 -0800

    Work in Progress

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000, () => { {
+  console.log("Server started (http://localhost:3000/) !");
+}
+});
+
+
+//Test to make sure the application works
+
+app.get("/", (req, res) => { {
+    res.render("index");
+  }});
